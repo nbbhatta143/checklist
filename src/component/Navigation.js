7fba7c9f21bd963cc0e168bd94d0d8ee99cc4cd3
nbbhatta143 <61528359+nbbhatta143@users.noreply.github.com>
2023-03-01 00:37:32 -0500

    added profile page and checklist page with beautiful css

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,22 +1,28 @@
 import { Component } from "react";
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./nav.css";
 
 class Navigation extends Component {
   render() {
     return (
-      <div>
-        <nav>
-          <ul id="navigation">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link className="link" to="/">
+                Home
+              </Link>
+              <Link className="link" to="/profile">
+                Profile
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
   }
 }
